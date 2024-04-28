480cac7cb561d7f0d92ea34cb40c795336d3047c
Sree201 <sreesirinath4@gmail.com>
2024-04-28 09:31:13 -0700

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Declearing  variable
 PERSON1=Ramesh #no spaace between = and value
 PERSON2=Suresh
 
+#referring variable
 echo "$PERSON1:: Hello $PERSON2 How r u"
 echo "$PERSON2:: Hello $PERSON1 I am good"
 echo "$PERSON1: I am fine How is your work"
 echo "$PERSON2:: not bad i am thinking to learn"
+

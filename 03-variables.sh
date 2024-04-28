b82c477b72fccf3c6ab33e7b88774e71126208af
Sree201 <sreesirinath4@gmail.com>
2024-04-27 18:42:44 -0700

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh #no spaace between = and value
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2 How r u"
+echo "$PERSON2:: Hello $PERSON1 I am good"
+echo "$PERSON1: I am fine How is your work"
+echo "$PERSON2:: not bad i am thinking to learn"

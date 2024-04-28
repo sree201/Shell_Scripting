50ed577385cf4fd9c328c1fed8b79636a3f5309f
Sree201 <sreesirinath4@gmail.com>
2024-04-28 09:31:28 -0700

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,4 +9,4 @@ echo "$PERSON1:: Hello $PERSON2 How r u"
 echo "$PERSON2:: Hello $PERSON1 I am good"
 echo "$PERSON1: I am fine How is your work"
 echo "$PERSON2:: not bad i am thinking to learn"
-
+echo "$PERSON1:: I am learning Kubernetes and DevOps project in this using 

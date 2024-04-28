518d0f3d3aa196bc468554267cbecdb204f272c6
Sree201 <sreesirinath4@gmail.com>
2024-04-27 18:36:10 -0700

    hello script

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Ramesh How r u"
+echo "Suresh:: Hello Suresh I am good"
+echo "Ramesh: I am fine How is your work"
+echo "Suresh:: not bad i am thinking to learn"

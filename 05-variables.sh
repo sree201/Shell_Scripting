88ffdbf72ade18f3d522d05b032074ed08079d4c
Sree201 <sreesirinath4@gmail.com>
2024-04-29 17:33:33 -0700

    hello script

--- 05-variables.sh
+++ 05-variables.sh
@@ -8,4 +8,4 @@ echo "Enter the password::"
 
 read -s PASSWORD #here password is variable
 
-echo "username is: $USERNAME, password is: $PASSWORD"
+echo -s "username is: $USERNAME, password is: $PASSWORD"

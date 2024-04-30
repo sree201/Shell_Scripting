612de8b9f07ceac88b92d030c2b6cff75967f961
Sree201 <sreesirinath4@gmail.com>
2024-04-29 17:21:27 -0700

    hello script

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,6 +7,6 @@ PERSON2=$2
 #referring variable
 echo "$PERSON1:: Hello $PERSON2 How r u"
 echo "$PERSON2:: Hello $PERSON1 I am good"
-echo "$PERSON1: I am fine How is your work"
+echo "$PERSON1:: I am fine How is your work"
 echo "$PERSON2:: not bad i am thinking to learn"
 echo "$PERSON1:: I am learning Kubernetes and DevOps project in this using 

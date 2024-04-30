5c39a6a4ce5cfa89a0eeeb822c956a45ba904226
Sree201 <sreesirinath4@gmail.com>
2024-04-29 20:04:15 -0700

    hello script

---
+++ 08-special-vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "All variables: $@"
+
+echo "Number of variables passed: $#"
+
+echo "Script name: $0"
+
+echo "Current working directory: $PWD"
+
+echo "Home directory of current user: $HOME"
+
+echo "Which user is running the script: $USER"
+
+echo "HOSTNAME: $HOSTNAME"
+
+echo "Process Id of the present current shell script: $$"

9aa4b88cebe5f6ae72a511d7448d9f10f8cc4e90
Sree201 <sreesirinath4@gmail.com>
2024-04-30 20:11:36 -0700

    hello script

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,8 +3,13 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    echo "Exit status: $1"
-    echo "How r u doing"
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+fi
 }
 
 if [ $USERID -ne 0 ]

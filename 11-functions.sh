b9db9eceb548008448a83601659d0daccf2472f2
Sree201 <sreesirinath4@gmail.com>
2024-04-30 20:03:26 -0700

    hello script

---
+++ 11-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    echo "Exit status: $1"
+    echo "How r u doing"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this root user access"
+    exit 1 #Manually exit if error comes
+else
+    echo "Your super user."
+fi
+ 
+dnf install mysql-selinux.noarch -y
+VALIDATE $? installing mysql-selinux.noarch"
+
+dnf install git -y 
+VALIDATE $? "installing mysql-selinux.noarch"

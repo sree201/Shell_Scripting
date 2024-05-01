2ebff1f11bc51ea6b13b931596b47a1e796dd157
Sree201 <sreesirinath4@gmail.com>
2024-04-30 21:47:15 -0700

    hello script

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
- SCRIPT_NAME=$($0 | cut -d "." -f1)
+ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
  LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
@@ -23,8 +23,8 @@ else
     echo "Your super user."
 fi
  
-dnf install mysql-selinux.noarch -y &>>LOGFILE
+dnf install mysql-selinux.noarch -y &>>$LOGFILE
 VALIDATE $? "installing mysql-selinux.noarch"
 
-dnf install git -y &>>LOGFILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing Git"

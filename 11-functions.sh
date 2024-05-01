1ccfd553c3eebd4f6dfec7b87080eabae39b616a
Sree201 <sreesirinath4@gmail.com>
2024-04-30 22:04:46 -0700

    hello script

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,14 +4,19 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
  SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
  LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+ R="\e[31m"
+ G="\e[32m"
+ N="\e[33m"
+
+ echo "Script started executed at: $TIMESTAMP"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS $N"
 fi
 }
 
@@ -28,3 +33,6 @@ VALIDATE $? "installing mysql-selinux.noarch"
 
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing Git"
+
+dnf install some-wrong -y &>>LOGFILE
+VALIDATE $? INSTALLING logfile

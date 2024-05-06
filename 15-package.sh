1343a3bc88d16c0f9d29e293308df347e1752d08
Sree201 <sreesirinath4@gmail.com>
2024-05-06 13:50:19 -0700

    install

--- 15-package.sh
+++ 15-package.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 
 
-if [$USERID -ne 0] #"0" contains username
+if [ $USERID -ne 0 ] # 0 contains script name
 then 
     echo "Install package with root access"
     exit 1 
@@ -13,7 +13,7 @@ fi
 
 for i in $0 # Looping all the parameters what you have $i
 do
-    ehco "Package to install: $i"
+    echo "Package to install: $i"
     dnf list installed packages $i # check the exit status "we use if condition" or we use validate function"
 if [ $? -eq 0 ] # "$?" Special variable for exit status
 then

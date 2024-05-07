b29f56ede45a672f1202a28338333a68ffd1aa1b
Sree201 <sreesirinath4@gmail.com>
2024-05-07 14:33:02 -0700

    Install-packages

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -29,9 +29,8 @@ else
 fi
 
 for i in $0
-do
     echo "package to install: $i"
-    dnf list install -y &>>$LOGFILE  # check the exit status "we use if condition" "or we can use Validate function"
+    dnf list install mysql-selinux.noarch -y &>>$LOGFILE  # check the exit status "we use if condition" "or we can use Validate function"
     if [ $? -eq 0 ]
     then
         echo -e "$i already installed...$Y SKIPPING  $N"

cea35727bee228d874bd23b6c30b24aba95f6601
Sree201 <sreesirinath4@gmail.com>
2024-05-07 13:30:19 -0700

    install

--- 16-new-Packages.sh
+++ 16-new-Packages.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-USERID=$(id -u)
+source ./common.sh
+
+echo "Enter MySQL DB Password:"
+read -s mysql_root_password # ExpenseApp@1
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling default NodeJS"
+
+dnf module enable nodejs:20 -y &>> $LOGFILE
+VALIDATE $? "Enabling NodeJS:20"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing NodeJS:20"
+
+id expense &>> $LOGFILE
+
+if [ $? -ne 0 ]
+then
+    useradd expense &>> $LOGFILE
+    VALIDATE $? "Adding User: expense"
+else
+    echo -e "User expense is already present...$Y SKIPPED $W"
+fi
+
+mkdir -p /app &>> $LOGFILE
+
+curl -o /tmp/backend.zip "https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip" &>> $LOGFILE
+VALIDATE $? "Downloading Source Code"
+
+rm -r /app/*
+cd /app
+unzip /tmp/backend.zip &>> $LOGFILE
+VALIDATE $? "Unzipping the file"
+
+npm install &>> $LOGFILE
+VALIDATE $? "Installing NodeJS dependencies"
+
+# CODE COMMENTED FOR READABILITY

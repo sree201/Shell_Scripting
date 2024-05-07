31aa5cdb052adf96c7a85831fa5413ee5fe1cdb8
Sree201 <sreesirinath4@gmail.com>
2024-05-07 14:33:19 -0700

    Install-packages

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -29,6 +29,7 @@ else
 fi
 
 for i in $0
+do
     echo "package to install: $i"
     dnf list install mysql-selinux.noarch -y &>>$LOGFILE  # check the exit status "we use if condition" "or we can use Validate function"
     if [ $? -eq 0 ]

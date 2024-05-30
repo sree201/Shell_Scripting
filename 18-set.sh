213f3d3e4864ae601f93e24dba832e9b22c875ae
Sree201 <sreesirinath4@gmail.com>
2024-05-29 18:48:21 -0700

    current-script

--- 18-set.sh
+++ 18-set.sh
@@ -1,19 +1,1 @@
 #!/bin/bash
-
-set -e 
-
-USERID=$(id -u)
-
-if  [ $USERID -ne 0 ]
-then 
-    echo "please run the script with root access."
-    exit 1
-else
-    echo "your are a super user."
-fi
-
-dnf install mysql -y
-dnf install git -y
-
-
-echo "is script processing"
